79a6f3d62841ac0524d5c5fc86879707ec84e6d0
Alex Aquino <aaquino1223@gmail.com>
2021-04-03 16:39:09 -0400

    Added initial Player Controller
    Added PlayerController and logic to prevent player from going off game screen.

---
+++ SafeSurfing/Assets/Safe Surfing/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static SafeSurfing.Common.Constants.PlayerInput;
+
+namespace SafeSurfing
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        public float Speed = 5f;
+        public float FallSpeed = 2.5f;
+
+        private Rigidbody2D _RigidBody;
+
+        public bool IsMoving { get { return _Horizontal != 0 || _Vertical != 0; } }
+        private float _Horizontal;
+        private float _Vertical;
+        private float _InitialGravityScale;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _RigidBody = GetComponent<Rigidbody2D>();
+            _InitialGravityScale = _RigidBody.gravityScale;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _Horizontal = IsPressingLeft ? -1 : (IsPressingRight ? 1 : 0);
+
+            _Vertical = IsPressingDown ? -1 : ((IsPressingUp || IsPressingSpace) ? 1 : 0);
+        }
+
+        private void FixedUpdate()
+        {
+            if (IsMoving)
+            {
+                _RigidBody.velocity = new Vector2(_Horizontal * Speed, _Vertical * Speed);
+                _RigidBody.gravityScale = 0;
+            }
+            else
+            {
+                _RigidBody.velocity = new Vector2(0f, -FallSpeed);
+                _RigidBody.gravityScale = _InitialGravityScale;
+            }
+        }
+    }
+
+    
+}
---
+++ SafeSurfing/Assets/Safe Surfing/Scripts/PolygonToEdgeColliderConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PolygonToEdgeColliderConverter : MonoBehaviour
+{
+    private void Awake()
+    {
+        var polygonCollider = GetComponent<PolygonCollider2D>();
+
+        if (polygonCollider == null)
+            polygonCollider = gameObject.AddComponent<PolygonCollider2D>();
+
+        var edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+
+        var points = polygonCollider.points.ToList();
+        points.Add(points[0]); //Add first point for final edge
+
+        edgeCollider.points = points.ToArray();
+        Destroy(polygonCollider);
+    }
+}
