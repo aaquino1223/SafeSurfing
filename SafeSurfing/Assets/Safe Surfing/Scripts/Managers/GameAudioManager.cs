4f4c299256205b3da874e485f9e035fffb213716
Kwan Holloway <kwansholloway@gmail.com>
2021-04-05 18:56:32 -0400

    Temporary Start Screen and game Music
    Added a temp start screen that fades out music on game start and temp game music that fades in

---
+++ SafeSurfing/Assets/Safe Surfing/Scripts/Managers/GameAudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource _StageMusic;
+    void Start()
+    {
+        _StageMusic = GetComponent<AudioSource>();
+        if(_StageMusic != null){
+            _StageMusic.volume = 0f;
+            StartCoroutine(FadeAudioSource.StartFade(_StageMusic, 1f, 1f));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SafeSurfing/Assets/Safe Surfing/Scripts/Managers/StartMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using static SafeSurfing.Common.Constants.PlayerInput;
+
+
+public class StartMenuManager : MonoBehaviour
+{
+    private AudioSource _TitleTheme;
+    public GameObject PlayableGame;
+    private float _TimeBeforeStart = 1.75f;
+    private CanvasGroup _MenuCanvasGroup;
+    private Animator _Transition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _TitleTheme = GetComponent<AudioSource>();
+        _Transition = GetComponent<Animator>();
+        _MenuCanvasGroup = GetComponent<CanvasGroup>();
+        _TitleTheme.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPressingSpace){
+            StartCoroutine(FadeAudioSource.StartFade(_TitleTheme, _TimeBeforeStart, 0f));
+            _Transition.SetBool("Open", false);
+            StartCoroutine(StartGame());
+        }
+                
+    }
+
+    private IEnumerator StartGame(){
+
+        yield return new WaitForSeconds(_TimeBeforeStart);
+
+        PlayableGame.SetActive(true);
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
+    }
+}
